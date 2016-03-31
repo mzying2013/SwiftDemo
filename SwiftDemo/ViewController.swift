8f92b2a69fe9ab55ef2c80bf0a93965a4c56721c
Bill liu <317590898@qq.com>
2016-03-31 17:36:53 +0800

    常量学习

--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -13,6 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let maximumNumberOfLoginAttempts = 20
+//        var currentLoginAttempts = 0
+        
+        var x = 0.0, y = 0.0, z = 0.0
+        x += y + z
+        
+        var welcomeMessage : String
+        welcomeMessage = "Hello world!"
+        
+//        let π = 3.1415926
+//        let 你好 = "你好，世界"
+//        let 🐶 = "lovely dog"
+        
+        var friendlyWelcome = "Hello!"
+        print(friendlyWelcome,terminator:"")
+        friendlyWelcome = "Bonjour!"
+        print(friendlyWelcome)
+        
+        print("The current value of 1 - \(welcomeMessage) 2 - \(friendlyWelcome)")
+        
+        
+        var dd : Double
+        dd = 4.1112345
+        var ii : Int
+        ii = 12345
+        print("dd: \(dd)  ii: \(ii)")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
